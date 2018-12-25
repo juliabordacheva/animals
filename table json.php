247c4185c71ca4bd4d8ad4fb188000bff8aef458
Юлия Бордачева <s_proza@list.ru>
2018-12-25 19:47:44 +0300

    2commit

--- table json.php
+++ table json.php
@@ -7,23 +7,7 @@ $data = file_get_contents(__DIR__ . '/table.json');
 
 $contacts = json_decode($data, true);
 
-/*var_dump($contacts);*/
 
-/*$contacts = [
-   ['firstName' => 'Иван', 'lastName' => 'Иванов', 'address' => 'г. Москва, ул. Алиева,2', 'phone' => '8(926)111-11-11'],
-   ['firstName' => 'Максим', 'lastName' => 'Петров', 'address' => 'г. Клинцы, ул. Сталина,2', 'phone' => '8(926)222-22-22'],
-   ['firstName' => 'Елена', 'lastName' => 'Сидорова', 'address' => 'г. Нижневартовск, ул. Ленина,2','phone' => '8(926)333-33-33'],
-   ['firstName' => 'Василиса', 'lastName' => 'Козлова', 'address' => 'г. Тверь, ул. Комсомольцев,2', 'phone' => '8(926)444-44-44'],
-   ['firstName' => 'Эдуард', 'lastName' => 'Шереметьев', 'address' => 'г. Владивосток, ул. Нижняя,2', 'phone' => '8(926)555-55-55'],
-   ['firstName' => 'Артем', 'lastName' => 'Ищенко', 'address' => 'г. Москва, ул. Верхняя,2', 'phone' => '8(926)666-66-66']
-
-];*/
-
-
-
-/*echo "<pre>".json_encode($contacts, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)."</pre>";*/
-
-/*exit;*/
 
 ?>
 
